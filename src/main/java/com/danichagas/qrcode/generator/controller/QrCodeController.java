4aaa400667b2d9e1d417535f6bb926e845334da6
danichagas <chagasdaniel788@gmail.com>
2025-12-05 23:33:40 -0300

    feat: Initializing the method that generates the QR code.

---
+++ src/main/java/com/danichagas/qrcode/generator/controller/QrCodeController.java
@@ -0,0 +1,20 @@
+package com.danichagas.qrcode.generator.controller;
+
+import com.danichagas.qrcode.generator.dto.QrCodeGenerateRequest;
+import com.danichagas.qrcode.generator.dto.QrCodeGenerateResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/qrcode")
+public class QrCodeController {
+
+    @PostMapping()
+    public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) {
+        return null;
+    }
+
+}
