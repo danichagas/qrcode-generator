0396dd8a8a09191b8e25058943339dcd0a32e3d3
danichagas <chagasdaniel788@gmail.com>
2025-12-09 23:11:32 -0300

    feat: creating the QR code creation service

--- src/main/java/com/danichagas/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/danichagas/qrcode/generator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.danichagas.qrcode.generator.controller;
 
 import com.danichagas.qrcode.generator.dto.QrCodeGenerateRequest;
 import com.danichagas.qrcode.generator.dto.QrCodeGenerateResponse;
+import com.danichagas.qrcode.generator.services.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeService) {
+        this.qrCodeGeneratorService = qrCodeService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) {
-        return null;
+        try {
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+
     }
 }
